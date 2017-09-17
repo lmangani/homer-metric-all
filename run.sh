ca1f548e3235e536b463f5bccf500a7f097d3e7f
Lorenzo <lorenzo.mangani@gmail.com>
2017-09-17 08:22:33 -0400

    fix target patching

--- run.sh
+++ run.sh
@@ -187,7 +187,7 @@ if [ -n "$DO_INFLUXDB" ]; then
 	sed -i '/^#.*define.*DO_INFLUXDB/s/^#//' /etc/kamailio/kamailio.cfg
 fi
 if [ -n "$INFLUXDB_URL" ]; then
-	sed -i -e "s/127.0.0.1:8086/$DO_INFLUX_URL/g" /etc/kamailio/kamailio.cfg
+	sed -i -e "s/127.0.0.1:8086/${INFLUX_URL}/g" /etc/kamailio/kamailio.cfg
 fi
 ##################
 ## ELASTICSEARCH OPTIONS
@@ -195,7 +195,7 @@ if [ -n "$DO_ELASTICSEARCH" ]; then
 	sed -i '/^#.*define.*DO_ELASTICSEARCH/s/^#//' /etc/kamailio/kamailio.cfg
 fi
 if [ -n "$ELASTICSEARCH_URL" ]; then
-	sed -i -e "s/127.0.0.1:9200/$DO_ELASTICSEARCH_URL/g" /etc/kamailio/kamailio.cfg
+	sed -i -e "s/127.0.0.1:9200/${ELASTICSEARCH_URL}/g" /etc/kamailio/kamailio.cfg
 fi
 ##################
 ## GRAYLOG OPTIONS
